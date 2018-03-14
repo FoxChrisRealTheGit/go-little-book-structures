859c22b91cbe05ac389f8c64b37e6d56f1bff92a
Christopher Fox <cfox1290@gmail.com>
2018-03-13 19:42:44 -0600

    done with chapter

---
+++ go/src/constructors.go
@@ -0,0 +1,32 @@
+package main
+
+// import (
+// 	"fmt"
+// )
+
+//  type Saiyan struct{
+// 		Name string
+// 		Power int
+// 		Father *Saiyan
+// 	}
+
+
+// func NewSaiyan(name string, power int) *Saiyan{
+// 	return &Saiyan{
+// 		Name: name,
+// 		Power: power,
+// 	}
+// }
+
+// func main(){
+// 	gohan := &Saiyan{
+// 		Name: "Gohan",
+// 		Power: 1000,
+// 		Father: &Saiyan{
+// 			Name: "Goku",
+// 			Power: 9001,
+// 			Father: nil,
+// 		},
+// 	}
+// 	fmt.Println(gohan.Power)
+// }
---
+++ go/src/structures.go
@@ -0,0 +1,28 @@
+package main
+
+// import(
+// 	"fmt"
+// )
+
+
+// type Saiyan struct{
+// 	Name string
+// 	Power int
+// }
+
+// func (s *Saiyan) Super(){
+// 	s.Power +=10000
+// }
+
+// func main(){
+// goku := &Saiyan{"Goku",9000}
+// // Super(goku)
+// goku.Super()
+// fmt.Println(goku.Power)
+// }
+
+
+// func Super(s *Saiyan){
+// 	s.Power+=10000
+// }
+
